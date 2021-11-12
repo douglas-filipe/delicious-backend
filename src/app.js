c3e42d51d13c6db2aecdefbbc748c5862fc927ef
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-11-12 18:05:51 -0300

    feat: upload route completed

--- src/app.js
+++ src/app.js
@@ -1,55 +1,59 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const User = require('./routes/user')
-const Recipe = require("./routes/recipe")
-const Comment = require("./routes/comment")
-const swaggerDocs = require("./swagger.json")
-const swaggerUi = require('swagger-ui-express')
-const mongoose = require('mongoose')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const User = require("./routes/user");
+const uploadImage = require("./routes/upload");
+const Recipe = require("./routes/recipe");
+const Comment = require("./routes/comment");
+const swaggerDocs = require("./swagger.json");
+const swaggerUi = require("swagger-ui-express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
-mongoose.connect(process.env.MONGO)
-  .then(()=>{
-    console.log("DB running")
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("DB running");
   })
-  .catch((e)=>{
-    console.log(e)
-  })
+  .catch((e) => {
+    console.log(e);
+  });
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-dotenv.config()
+dotenv.config();
 
 const DisableTryItOutPlugin = function () {
-    return {
-      statePlugins: {
-        spec: {
-          wrapSelectors: {
-            allowTryItOutFor: () => () => false,
-          },
+  return {
+    statePlugins: {
+      spec: {
+        wrapSelectors: {
+          allowTryItOutFor: () => () => false,
         },
       },
-    };
-  };
-  
-  const options = {
-    swaggerOptions: {
-      plugins: [DisableTryItOutPlugin],
     },
   };
-app.get("/", (req, res) => {
-  res.send("Está funcionando")
-})
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, options));  
-app.use("/users", User)
-app.use("/recipe", Recipe)
-app.use("/comment", Comment)
+};
 
-app.use("/uploads", express.static('uploads'))
+const options = {
+  swaggerOptions: {
+    plugins: [DisableTryItOutPlugin],
+  },
+};
+app.get("/", (req, res) => {
+  res.send("Está funcionando");
+});
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, options));
+app.use("/api", uploadImage.routes);
+app.use("/users", User);
+app.use("/recipe", Recipe);
+app.use("/comment", Comment);
+
+app.use("/uploads", express.static("uploads"));
 
 app.listen(process.env.PORT || 3001, () => {
-    console.log("App running: " + 3001)
-})
+  console.log("App running: " + 3001);
+});
---
+++ src/controllers/uploadController.js
@@ -0,0 +1,30 @@
+const firebase = require("../firebaseApp");
+require("firebase/compat/storage");
+
+const storage = firebase.storage().ref();
+
+global.XMLHttpRequest = require("xhr2");
+
+const addImage = async (req, res) => {
+  try {
+    const file = req.file;
+
+    const timestamp = Date.now();
+    const name = file.originalname.split(".")[0];
+    const type = file.originalname.split(".")[1];
+    const fileName = `${name}_${timestamp}.${type}`;
+
+    const imageRef = storage.child(fileName);
+    const snapshot = await imageRef.put(file.buffer);
+    const downloadURL = await snapshot.ref.getDownloadURL();
+    res.status(201).json({ url: downloadURL });
+    //res.send(downloadURL);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err.message);
+  }
+};
+
+module.exports = {
+  addImage,
+};
